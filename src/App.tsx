f4de457d3ab7d97a37570a46d4686cefd898b492
appdevwaleed <125948234+appdevwaleed@users.noreply.github.com>
2024-08-08 15:32:24 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="javascript:void(0)">
+            Logo
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mynavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="mynavbar">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0)">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0)">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="javascript:void(0)">
+                  Link
+                </a>
+              </li>
+            </ul>
+            {/* <form className="d-flex">
+        <input className="form-control me-2" type="text" placeholder="Search">
+        <button className="btn btn-primary" type="button">Search</button>
+      </form> */}
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
