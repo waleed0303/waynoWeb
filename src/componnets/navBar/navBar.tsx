90e581df3c8c7e8c0c8989b34ec8aa81aea66e99
appdevwaleed <125948234+appdevwaleed@users.noreply.github.com>
2024-08-27 16:44:00 +0400

    changes

--- src/componnets/navBar/navBar.tsx
+++ src/componnets/navBar/navBar.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./Navbar.css"; // Import CSS file for styling
-import { FaUser } from "react-icons/fa"; // Import the user icon from Font Awesome
+import { FaUser, FaChevronDown, FaChevronUp } from "react-icons/fa"; // Import the user icon from Font Awesome
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [activeLink, setActiveLink] = useState("");
+  const [activeLink, setActiveLink] = useState("home");
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
@@ -15,80 +15,96 @@ const Navbar = () => {
   };
   const handleClick = (section: any) => {
     setActiveLink(section);
+    setIsDropdownOpen(false);
   };
 
   return (
-    <nav className="navbar">
+    <nav className={`${isOpen ? "navbarOpened" : "navbar"}`}>
       <div className="navbar-logo">
         <img
-          src="https://images.wayno.ae/insecure/fit/150/92/sm/0/plain/https://runrunuae-assets.s3.me-central-1.amazonaws.com/Clientlogo/65965c448b897.png"
+          src="https://images.wayno.ae/insecure/fit/150/92/sm/0/plain/https://runrunuae-assets.s3.me-central-1.amazonaws.com/Clientlogo/66cd80617f1c6.png"
           className="photo"
           width="150"
-          height="30"
-        ></img>
+          height="30"></img>
       </div>
 
-      <ul className={`navbar-links`}>
+      <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
         <li>
           <a
             href="#home"
-            className={activeLink === "home" ? "active" : ""}
-            onClick={() => handleClick("home")}
-          >
+            className={activeLink === "home" ? "active normal" : "normal"}
+            onClick={() => handleClick("home")}>
             HOME
           </a>
         </li>
         <li>
           <a
             href="#courier"
-            className={activeLink === "courier" ? "active" : ""}
-            onClick={() => handleClick("courier")}
-          >
+            className={activeLink === "courier" ? "active normal" : "normal"}
+            onClick={() => handleClick("courier")}>
             COURIER
           </a>
         </li>
         <li>
           <a
             href="#recoveryPickup"
-            className={activeLink === "recoveryPickup" ? "active" : ""}
-            onClick={() => handleClick("recoveryPickup")}
-          >
+            className={
+              activeLink === "recoveryPickup" ? "active normal" : "normal"
+            }
+            onClick={() => handleClick("recoveryPickup")}>
             RECOVERY & PICKUP
           </a>
         </li>
         <li>
           <a
             href="#contact"
-            className={activeLink === "contact" ? "active" : ""}
-            onClick={() => handleClick("contact")}
-          >
+            className={activeLink === "contact" ? "active normal" : "normal"}
+            onClick={() => handleClick("contact")}>
             Contact
           </a>
         </li>
-        <li className="dropdown">
+        <li>
           <a
             href="#user"
             className={activeLink === "user" ? "active " : ""}
-            onClick={() => toggleDropdown("user")}
-          >
-            <FaUser className="user-icon" />
+            onClick={() => toggleDropdown("user")}>
+            <FaUser /> <span style={{ marginLeft: 5 }}>Account</span>{" "}
+            {isDropdownOpen ? (
+              <FaChevronUp style={{ marginLeft: 5 }} />
+            ) : (
+              <FaChevronDown style={{ marginLeft: 5 }} />
+            )}
           </a>
 
           {isDropdownOpen && (
-            <div className="dropdown">
-              <div className="dropdown-content">
-                <a href="#">Login</a>
-                <a href="#">Sign up</a>
-              </div>
-            </div>
+            <ul className="dropdown-content">
+              <li>
+                <a
+                  className={activeLink === "login" ? "active " : ""}
+                  href="#web-design"
+                  onClick={() => handleClick("login")}>
+                  Login
+                </a>
+              </li>
+              <li>
+                <a
+                  className={activeLink === "signup" ? "active " : ""}
+                  href="#web-design"
+                  onClick={() => handleClick("signup")}>
+                  Signup
+                </a>
+              </li>
+            </ul>
           )}
         </li>
       </ul>
-      <div className="navbar-toggle" onClick={toggleMenu}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
+      {isOpen == false && (
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      )}
     </nav>
   );
 };
